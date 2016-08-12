5c229024892227548739638a9480afd5661c2d8e
Martono Wibowo <martonowibowo@gmail.com>
2016-08-12 11:30:04 +0700

    delete docker_var

--- first_run.sh
+++ first_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 pathresult=~/docker/docker_var/
-sudo docker run -t -i -d -p 80:80 -p 3306:3306 --name magento -v ~/docker/docker_var/:/mnt/ martonowibowo/centos6:magentoapp
+tar zxvf  docker_var.tar.gz
+chmod 777 -R docker_var
+sudo docker run -t -i -d -p 80:80 -p 3306:3306 --name magento -v ~/docker/docker_var/:/mnt/ martonowibowo/centos:magentoapp
 sleep 10
 sudo docker exec magento sh /start.sh
 echo "you can put your project in ~/docker/docker_var/public_html/"
