b56e19b9af7d202bdf340736ab518b0989980828
Martono Wibowo <martonowibowo@gmail.com>
2016-08-19 15:01:07 +0700

    fixing first run

--- first_run.sh
+++ first_run.sh
@@ -3,8 +3,8 @@
 pathresult=~/docker/docker_var/
 tar zxvf  docker_var.tar.gz
 chmod 777 -R docker_var
-sudo docker run -t -i -d -p 80:80 -p 3306:3306 --name magento -v ~/docker/docker_var/:/mnt/ martonowibowo/centos:magentoapp
-sleep 10
-sudo docker exec magento sh /start.sh
+sudo docker run -t -i -d -p 80:80 -p 3306:3306 --name magento -v ~/docker/docker_var/:/mnt/ martonowibowo/centos:magentoapp sh /start.sh
+#sleep 10
+#sudo docker exec magento sh /start.sh
 echo "you can put your project in ~/docker/docker_var/public_html/"
 
