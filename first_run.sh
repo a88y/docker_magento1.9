172ac4d14541531806aa136ce4601d48a98b793b
Martono Wibowo <martonowibowo@gmail.com>
2016-09-22 12:06:13 +0700

    Update first_run.sh
    update for support in mac

--- first_run.sh
+++ first_run.sh
@@ -2,7 +2,7 @@
 
 pathresult=~/docker/docker_var/
 tar zxvf  docker_var.tar.gz
-chmod 777 -R docker_var
+chmod -R 777 docker_var
 sudo docker run -t -i -d -p 80:80 -p 3306:3306 --name magento -v ~/docker/docker_var/:/mnt/ martonowibowo/centos:magentoapp
 sleep 10
 sudo docker exec magento sh /start.sh
